4011262676e5d99917e9572bcb35f3b2019a3981
Yury <quasaryy@gmail.com>
2023-01-12 19:37:28 +0400

    Added main screen and segue to dashboard screen

--- LoginApp/ViewControllers/ViewController.swift
+++ LoginApp/ViewControllers/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var passwordTF: UITextField!
+    @IBOutlet var usernameTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func forgotPasswordButton() {
+    }
+    
+    @IBAction func loginButton() {
+    }
+    
+    @IBAction func forgotUsernameButton() {
+    }
 }
 
