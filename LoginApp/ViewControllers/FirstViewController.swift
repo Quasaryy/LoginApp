39d0a919b01393b7b7b9b7f53a29f45960bc1b0e
Yury <quasaryy@gmail.com>
2023-01-13 00:51:35 +0400

    Added Tab Bar Controller and all logic

--- LoginApp/ViewControllers/FirstViewController.swift
+++ LoginApp/ViewControllers/FirstViewController.swift
@@ -55,3 +55,19 @@ extension ViewController {
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == usernameTF {
+            passwordTF.becomeFirstResponder()
+        } else {
+            loginTapped()
+        }
+        return true
+    }
+}
+
